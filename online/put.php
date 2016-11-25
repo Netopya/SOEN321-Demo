78fd5c5e60d3674633962079b7d81bad73301826
Michael B <netopya@netopyaplanet.com>
2016-11-24 21:40:42 -0500

    Completed data collection
    -Can now send data to the put.php over GET or POST with the fingerprint,
    key, and value parameters

--- online/put.php
+++ online/put.php
@@ -7,18 +7,41 @@
     $key = $_REQUEST["key"];
     $value = $_REQUEST["value"];
     
+    //mysqli_report(MYSQLI_REPORT_ALL);
+
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
+
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $stmt = $conn->prepare("IF EXISTS (SELECT * FROM Prints WHERE fingerprint=? AND key=?)
-                                UPDATE Prints SET value=? WHERE fingerprint=? AND key=?
-                            ELSE
-                                INSERT INTO Prints (fingerprint, key, value) VALUES (?,?,?)");
-    $stmt->bind_param("ssssssss", $fingerPrint, $key, $value, $fingerPrint, $key, $fingerPrint, $key, $value);
-    
+    // Check for an existing print key pair
+    $stmt = $conn->prepare("SELECT * FROM Prints WHERE fingerprint=? AND partnerkey=?");
+    $stmt->bind_param("ss", $fingerPrint, $key);    
     $stmt->execute();
+    $stmt->store_result();
+
+    if($stmt->num_rows == 0)
+    {
+        // Insert the key and value into the database
+        $stmt = $conn->prepare("INSERT INTO Prints (fingerprint, partnerkey, kvalue) VALUES (?,?,?)");
+
+        if(!$stmt)
+        {
+            echo 'Error message ' . $mysqli->error;
+        }
+            
+
+        $stmt->bind_param("sss", $fingerPrint, $key, $value);    
+        $stmt->execute();
+    }
+    else
+    {
+        // Updating an already existing key-value pair
+        $stmt = $conn->prepare("UPDATE Prints SET kvalue=? WHERE fingerprint=? AND partnerkey=?");
+        $stmt->bind_param("sss", $value, $fingerPrint, $key); 
+        $stmt->execute();
+    }
 ?>
